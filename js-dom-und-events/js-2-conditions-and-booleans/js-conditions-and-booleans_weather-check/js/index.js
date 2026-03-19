63f15f095b5cbf39000b30188e39eab299c67f10
Olivia <oliviabelu@web.de>
2026-03-19 16:46:57 +0100

    solved weather task

---
+++ js-dom-und-events/js-2-conditions-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
@@ -0,0 +1,42 @@
+// Change this value to test different "weather" conditions.
+const weather = "sunny";
+switch (weather) {
+  case "sunny":
+    console.log("It's sunny!");
+    break;
+  case "rainy":
+    console.log("It's raining, don't forget your umbrella!");
+    break;
+  case "snowy":
+    console.log("It's snowing, stay warm!");
+    break;
+  default:
+    console.log("I'm not sure what the weather is like.");
+    break;
+}
+
+// Adjust the "temperature" value to trigger different messages (for Part 2 only).
+const temperature = -1;
+
+switch (weather) {
+  case "sunny":
+    if (temperature > 20) {
+      console.log("It's sunny and warm!");
+    } else {
+      console.log("It's sunny, but a bit chilly!");
+    }
+    break;
+  case "rainy":
+    console.log("It's raining, don't forget your umbrella!");
+    break;
+  case "snowy":
+    if (temperature < 0) {
+      console.log("Freezing snow!");
+    } else {
+      console.log("It's snowing, stay warm!");
+    }
+    break;
+  default:
+    console.log("I'm not sure what the weather is like.");
+    break;
+}
