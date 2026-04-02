1931796fd9991df22991d4b29ad414d6a50777f3
Olivia <oliviabelu@web.de>
2026-04-02 15:22:50 +0200

    solved error handling tasks

---
+++ 3013-js-error-handling/js-error-handling_calculator/js/index.js
@@ -0,0 +1,31 @@
+console.clear();
+
+const form = document.querySelector("form");
+const output = form.querySelector("output");
+const errorArea = document.querySelector('[class="error"]');
+
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero!");
+    }
+    return a / b;
+  },
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  errorArea.textContent = "";
+  const firstNumber = Number(event.target.firstNumber.value);
+  const secondNumber = Number(event.target.secondNumber.value);
+  const operation = event.target.operation.value;
+  try {
+    output.innerText = operations[operation](firstNumber, secondNumber);
+  } catch (error) {
+    console.log(errorArea);
+    errorArea.textContent = error;
+  }
+});
